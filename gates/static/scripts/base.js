daf33ac31ddfd61d298e2f26d64c2701c0e2aa2c
David Ma <davidmaamoaix@gmail.com>
2019-03-24 12:15:06 +0800

    Order page

--- gates/static/scripts/base.js
+++ gates/static/scripts/base.js
@@ -1,3 +1,7 @@
+$('article').on('click', function() {
+	$('.dropdown').slideUp();
+});
+
 $('#logout-toggle').on('click', function() {
 	$('#logout-menu').slideToggle();
 });
--- gates/static/scripts/orders.js
+++ gates/static/scripts/orders.js
@@ -4,3 +4,11 @@ $('document').ready(function() {
 	$('.welcome').fadeIn(2000);
 	$('.guide-text').fadeIn(2000);
 });
+
+$(function() {
+	$('.order-title').on('click', function() {
+		$('#' + $(this).attr('panel-id')).slideToggle(300);
+		var symbol = $(this).find('.expand-symbol').html();
+		$(this).find('.expand-symbol').html(symbol == '-' ? '+' : '-');
+	});	
+});
